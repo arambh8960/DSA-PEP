0dbe36bce949ad5d704ff75c75fbe03d9245d6c9
Arambh Tiwari <arambhtiwari@Arambhs-MacBook-Air.local>
2026-01-30 09:46:15 +0530

    string

---
+++ String.java
@@ -0,0 +1,42 @@
+https://leetcode.com/problems/is-subsequence/
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+     int i   =0;
+     int j=0;
+     while(i<s.length()&&j<t.length()){
+        if(s.charAt(i)==t.charAt(j)){
+            i++;
+            
+        }
+            j++;
+        
+     }
+     
+     return i==s.length();
+     
+     
+    }
+}
+
+https://www.geeksforgeeks.org/problems/find-first-repeated-character4108/1
+
+// User function Template for Java
+class Solution {
+    String firstRepChar(String s) {
+        // code here
+       HashSet<Character> set=new HashSet<>();
+     
+       for(int i=0;i<s.length();i++){
+           if(!set.contains(s.charAt(i))){
+               set.add(s.charAt(i));
+               
+           }else{
+               return String.valueOf(s.charAt(i));
+
+           }
+           
+       }
+       return "-1";
+    }
+}
