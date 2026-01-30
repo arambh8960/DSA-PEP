f53032d1e6bda09eab0b9f5946583234ebb69376
Arambh Tiwari <arambhtiwari@Arambhs-MacBook-Air.local>
2026-01-30 16:42:16 +0530

    sum

---
+++ Recursion/fibonaachi.java
@@ -0,0 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static int  fib(int n){
+       if(n==0) return 0;
+       if(n==1)return 1;
+        return fib(n-1)+fib(n-2);
+       
+        
+    }
+    public static void main(String[] args) {
+     int n=5;
+     
+     System.out.print(fib(n));
+    }
+}
