e24c86b165312eacf6f1af28449d18d7527cc41e
Arambh Tiwari <arambh.tiwari11@gmail.com>
2026-03-23 10:30:02 +0530

    AC

---
+++ BinarySearch/FindMinimum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        int res=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]>nums[n-1]){
+                low=mid+1;
+            }else{
+                res=mid;
+                high=mid-1;
+            }
+        }
+        return nums[res];
+    }
+}
