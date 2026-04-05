4219b91862f57ed5111dcb6d2fa2ad38e83bd37d
Arambh Tiwari <arambh.tiwari11@gmail.com>
2026-04-05 21:33:11 +0530

    hg

---
+++ KadanesAlgo/MaxProduct.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int bep=nums[0];
+        int ben=nums[0];
+        int ans=nums[0];
+        for(int i=1;i<nums.length;i++){
+            int p1=nums[i]*bep;
+            int p2=nums[i];
+            int p3=nums[i]*ben;
+            ben=Math.min(p2,Math.min(p1,p3));
+            bep=Math.max(Math.max(p1,p3),p2);
+            
+            
+            ans=Math.max(Math.max(ben,bep),ans);
+        }
+        return ans;
+    }
+}
