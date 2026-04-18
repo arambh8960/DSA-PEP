acae1696c5b9b94aa1f47896d85506cab3d49bbe
Arambh Tiwari <arambh.tiwari11@gmail.com>
2026-04-18 22:57:54 +0530

    4sum

---
+++ twoPointers/3sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> l=new ArrayList<>();
+        Arrays.sort(nums);
+       for(int i=0;i<nums.length;i++){
+         if (i > 0 && nums[i] == nums[i - 1]) continue;
+        int j=i+1;
+        int k=nums.length-1;
+        while(j<k){
+            int sum=nums[i]+nums[k]+nums[j];
+            if(sum==0){
+                l.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                //skip duplicates
+                while(j<k && nums[j]==nums[j+1])j++;
+                while(j<k && nums[k]==nums[k-1])k--;
+            }
+            if(sum<0){
+                j++;
+            }else{
+                k--;
+            }
+
+        }
+       }
+       return l;
+        
+        
+    }
+}
