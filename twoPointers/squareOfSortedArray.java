4dbc33981b06e4ab920398917dfeba49bab831d5
Arambh Tiwari <arambh.tiwari11@gmail.com>
2026-04-28 23:15:35 +0530

    String

---
+++ twoPointers/squareOfSortedArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] res=new int[nums.length];
+       int l=0;
+       int r=nums.length-1;
+       int i=nums.length-1;
+      while(l<=r){
+        if(Math.abs(nums[l])>Math.abs(nums[r])){
+            res[i--]=nums[l]*nums[l];
+            l++;
+        }else{
+            res[i--]=nums[r]*nums[r];
+            r--;
+        }
+       }
+       return res;
+    }
+}
